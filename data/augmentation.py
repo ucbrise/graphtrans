2049d6c5c79722557d1ccbb4e2cf6082578d6793
Zhanghao Wu <zhanghao.wu@outlook.com>
2021-02-22 07:01:56 -0800

    [minor] add mask node features, fix subgraph, wrong mask_eigval

--- data/augmentation.py
+++ data/augmentation.py
@@ -5,6 +5,8 @@ import shutil
 import torch
 from torch_geometric.data import InMemoryDataset, download_url, extract_zip
 from torch_geometric.io import read_tu_data
+from torch_geometric.utils import get_laplacian, to_dense_adj, dense_to_sparse, contains_self_loops, remove_self_loops
+from scipy.linalg import eigh
 
 from itertools import repeat, product
 import numpy as np
@@ -82,6 +84,7 @@ def subgraph(data, aug_ratio):
             continue
         idx_sub.append(sample_node)
         idx_neigh.union(set([n for n in edge_index[1][edge_index[0] == idx_sub[-1]]]))
+        idx_neigh -= set(idx_sub)
 
     idx_drop = [n for n in range(node_num) if not n in idx_sub]
     idx_nondrop = idx_sub
@@ -106,22 +109,73 @@ def subgraph(data, aug_ratio):
 
 def mask_nodes(data, aug_ratio):
     # TODO: This one need to be fixed for node attributes that are not encoded.
-    raise NotImplementedError
     node_num, feat_dim = data.x.size()
     mask_num = int(node_num * aug_ratio)
 
-    token = data.x.mean(dim=0)
+    # token = torch.zeros_like(data.x[0], dtype=torch.long)
     idx_mask = np.random.choice(node_num, mask_num, replace=False)
-    data.x[idx_mask] = torch.tensor(token, dtype=torch.float32)
+    data.x[idx_mask] = 0
 
     return data
 
+def mask_eigval(data, aug_ratio):
+    raise NotImplementedError
+    num_nodes = data.num_nodes
+    assert not contains_self_loops(data.edge_index)
+    assert data.edge_attr is None
+    
+    edge_index, edge_weight = get_laplacian(data.edge_index, num_nodes=num_nodes)
+    L = to_dense_adj(edge_index, edge_attr=edge_weight, max_num_nodes=num_nodes)[0]
+    print("num_node", num_nodes)
+    print(edge_index.size())
+    print(L)
 
+    eigval, eigvec = torch.symeig(L, eigenvectors=True)
+
+    mask_num = int(num_nodes * aug_ratio)
+    eigval_mask = np.random.choice(num_nodes, mask_num, replace=False)
+    eigval[eigval_mask] = 0
+
+
+    L = torch.matmul(eigvec, torch.matmul(eigval.diag_embed(), eigvec.transpose(-2, -1)))
+    L.masked_fill_(L.abs().lt(1), 0)
+    print(L)
+
+    edge_index = L.nonzero(as_tuple=False).t().contiguous()
+    edge_index, _ = remove_self_loops(edge_index)
+    print(edge_index.size())
+    print(edge_index)
+    # print(edge_index.size())
+    # print(edge_index)
+    # perm = idx_adj[edge_index[0], edge_index[1]] - 1
+    # print(perm)
+    # print((perm > 0).sum())
+    
+    data.edge_index = edge_index
+
+    return data
 
 AUGMENTATIONS = {
     "dnodes": drop_nodes,
     "pedges": permute_edges,
     "subgraph": subgraph,
     "mask_nodes": mask_nodes,
+    "mask_eigval": mask_eigval,
     "none": no_aug
 }
+
+if __name__ == "__main__":
+    from tqdm import tqdm
+    from ogb.graphproppred import PygGraphPropPredDataset, Evaluator
+    dataset = PygGraphPropPredDataset(name='ogbg-code', root='/data/zhwu/ogb')
+    split_idx = dataset.get_idx_split()
+    for data in tqdm(dataset[split_idx["train"]]):
+        edge_index = data.edge_index
+        # print(data.x)
+        # mask = edge_index[0] == edge_index[1]
+        # self_loop_num = mask.sum().item()
+        # assert self_loop_num == 0, self_loop_num
+        # Test
+        mask_eigval(data, 0.01)
+        break
+
