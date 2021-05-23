5f6640f876c3a54ba75f4973924874ae9de1895c
Zhanghao Wu <zhanghao.wu@outlook.com>
2021-05-23 11:14:14 -0700

    [major] add config for ppa baseline

--- dataset/__init__.py
+++ dataset/__init__.py
@@ -1,9 +1,11 @@
 from .code import CodeUtil
 from .mol import MolUtil
+from .ppa import PPAUtil
 
 DATASET_UTILS = {
     'ogbg-code': CodeUtil,
     'ogbg-code2': CodeUtil,
     'ogbg-molhiv': MolUtil,
     'ogbg-molpcba': MolUtil,
+    'ogbg-ppa': PPAUtil,
 }
